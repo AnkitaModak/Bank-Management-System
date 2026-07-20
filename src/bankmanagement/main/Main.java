8c92e9bece1ec6033e8f8e04757dbf6c9a5e9077
Ankita <ankita180206@gmail.com>
2026-07-20 12:46:20 +0530

    Implement main method for application startup
    Added main method to initialize Person objects and display their details.

--- src/bankmanagement/main/Main.java
+++ src/bankmanagement/main/Main.java
@@ -1,4 +1,13 @@
 package bankmanagement.main;
+import bankmanagement.people.Person;
+import java.util.*;
 //Starts the application and displays the menu.
 public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Person p1 = new Person();
+        Person p2 = new Person(sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextInt());
+        p1.displayDetails();
+        p2.displayDetails();
+    }
 }
