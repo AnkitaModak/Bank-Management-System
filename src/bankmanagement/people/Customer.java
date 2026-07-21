3e45860281a66abe5b7e19996869de6121efe92c
Ankita <ankita180206@gmail.com>
2026-07-21 08:10:59 +0530

    Update Customer.java

--- src/bankmanagement/people/Customer.java
+++ src/bankmanagement/people/Customer.java
@@ -1,6 +1,25 @@
 package bankmanagement.people;
 //Represents a bank customer.
-public class Customer {
-    private int customerID;
+public class Customer extends Person{
+    private String customerID;
+    public Customer(){
+        customerID = "not applicable;";
+    }
+    public Customer(String name,int age,String address,String phoneNo,String customerID){
+        super(name,address,phoneNo,age);
+        this.customerID = customerID;
+    }
+    public String getCustomerID(){
+        return customerID;
+    }
+    public void setCustomerID(String newCustomerID){
+        this.customerID = newCustomerID;
+    }
+    @Override
+    public void displayDetails(){
+        super.displayDetails();
+        System.out.println("Customer ID: "+customerID);
+    }
+
 }
 
