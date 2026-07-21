ed59cfe86e211ba72b8dcd47e10db6e93735c7a1
Ankita <ankita180206@gmail.com>
2026-07-21 08:12:36 +0530

    Update Main.java

--- src/bankmanagement/main/Main.java
+++ src/bankmanagement/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args){
         Person p1 = new Person("Ankita Modak","Tarakeswar","9648291829",20);
         Customer c1 = new Customer("Arnab Ganguly",23,"Takipur","9097692242","c01");
-        Person p2 = new Customer();//upcasting
+        Person p2 = new Customer();//upcasting(Dynamic Method Dispatch)
         c1.displayDetails();
         p1.displayDetails();
         p2.displayDetails();
