f0e22fe095014f3f3b4221c0f0c9ce404b30e323
Ankita <ankita180206@gmail.com>
2026-07-22 05:59:37 +0530

    Update Employee.java

--- src/bankmanagement/people/Employee.java
+++ src/bankmanagement/people/Employee.java
@@ -1,6 +1,44 @@
 package bankmanagement.people;
 //Represents a bank employee.
-public class Employee {
-    private int employeeID;
+public class Employee extends Person{
+    private String employeeID;
     private String designation;
+    private double salary;
+    public Employee(){
+        this.employeeID = "Not Available";
+        this.designation="Not Available";
+        this.salary = 0.0;
+    }
+    public Employee(String name,String address,String phoneNo,int age,String employeeID,String designation,double salary){
+        super(name,address,phoneNo,age);
+        this.employeeID = employeeID;
+        this.designation = designation;
+        this.salary =salary;
+    }
+    public String getEmployeeID(){
+        return employeeID;
+    }
+    public void setEmployeeID(String newEmployeeID){
+        this.employeeID = newEmployeeID;
+    }
+    public String getDesignation(){
+        return designation;
+    }
+    public void setDesignation(String newDesignation){
+        this.designation = newDesignation;
+    }
+    public double getSalary(){
+        return salary;
+    }
+    public void setSalary(double newSalary){
+        this.salary = newSalary;
+    }
+    @Override
+    public void displayDetails(){
+        super.displayDetails();
+        System.out.println("Employee's ID: "+ getEmployeeID());
+        System.out.println("Employee's designation: "+getDesignation());
+        System.out.println("Employee's salary: ₹"+ getSalary());
+    }
 }
+
