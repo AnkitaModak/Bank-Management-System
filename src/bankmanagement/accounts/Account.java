f0f904ef8000ad0227cde9e78f4aa597d9830e02
Ankita <ankita180206@gmail.com>
2026-07-23 09:27:26 +0530

    Update Account.java

--- src/bankmanagement/accounts/Account.java
+++ src/bankmanagement/accounts/Account.java
@@ -2,23 +2,63 @@ package bankmanagement.accounts;
 import bankmanagement.people.Customer;
 //Stores common account information and behavior.
 public class Account {
-    private int accNumber;
+    private String accNumber;
     private double balance;
     private String ifscCode;
-    private String branchCode;
+    private String branchName;
     private Customer accountHolder;
     public Account(){
-        this.accNumber = 0;
+        this.accNumber = "Not Available";
         this.balance = 0.0;
         this.ifscCode = "Not Available.";
-        this.branchCode = "Not Available.";
+        this.branchName = "Not Available.";
         this.accountHolder = null;
     }
-    public Account(int accNumber,double balance,String ifscCode,String branchCode,Customer accountHolder){
+    public Account(String accNumber,double balance,String ifscCode,String branchName,Customer accountHolder){
         this.accNumber = accNumber;
         this.balance = balance;
         this.ifscCode = ifscCode;
-        this.branchCode = branchCode;
+        this.branchName = branchName;
         this.accountHolder = accountHolder;
     }
-}   public get
+
+    public String getAccNumber() {
+        return accNumber;
+    }
+
+    public void setAccNumber(String newAccNumber) {
+        this.accNumber = newAccNumber;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public void setBalance(double newBalance){
+        this.balance = newBalance;
+    }
+    public String getIfscCode(){
+        return  ifscCode;
+    }
+    public void setIfscCode(String newIfscCode){
+        this.ifscCode = newIfscCode;
+    }
+    public String getBranchName(){
+        return branchName;
+    }
+    public void setBranchName(String newBranchName){
+        this.branchName = newBranchName;
+    }
+    public Customer getAccountHolder(){
+        return accountHolder;
+    }
+    public void setAccountHolder(Customer newAccountHolder) {
+        this.accountHolder = newAccountHolder;
+    }
+    public void displayDetails(){
+        System.out.println("AccountHolder Details: ");
+        getAccountHolder().displayDetails();
+        System.out.println("Account number: "+ getAccNumber());
+        System.out.println("IFSC Code: " + getIfscCode());
+        System.out.println("Branch Name: "+ getBranchName());
+        System.out.println("available balance: "+getBalance());
+    }
+}
