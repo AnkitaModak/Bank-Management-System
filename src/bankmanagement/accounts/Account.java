afcca132a9e58c77cad60bfeeb0c47b1a1378a3e
Ankita <ankita180206@gmail.com>
2026-07-23 09:07:46 +0530

    Enhance Account class with additional properties
    Added new fields and constructors to the Account class.

--- src/bankmanagement/accounts/Account.java
+++ src/bankmanagement/accounts/Account.java
@@ -1,6 +1,24 @@
 package bankmanagement.accounts;
+import bankmanagement.people.Customer;
 //Stores common account information and behavior.
 public class Account {
     private int accNumber;
     private double balance;
-}
+    private String ifscCode;
+    private String branchCode;
+    private Customer accountHolder;
+    public Account(){
+        this.accNumber = 0;
+        this.balance = 0.0;
+        this.ifscCode = "Not Available.";
+        this.branchCode = "Not Available.";
+        this.accountHolder = null;
+    }
+    public Account(int accNumber,double balance,String ifscCode,String branchCode,Customer accountHolder){
+        this.accNumber = accNumber;
+        this.balance = balance;
+        this.ifscCode = ifscCode;
+        this.branchCode = branchCode;
+        this.accountHolder = accountHolder;
+    }
+}   public get
