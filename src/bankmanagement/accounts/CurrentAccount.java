ed0fb1139ca5a4e194a64ee8f84d7272bc078cba
Ankita <ankita180206@gmail.com>
2026-07-26 11:06:55 +0530

    Update CurrentAccount.java

--- src/bankmanagement/accounts/CurrentAccount.java
+++ src/bankmanagement/accounts/CurrentAccount.java
@@ -7,12 +7,14 @@ public class CurrentAccount extends Account{
     private double withdrawalLimit;
     private int transactionLimit;
     private double maintenanceFee;
+    private int transactionCount;
     public CurrentAccount(){
         super();
         this.overdraftLimit = 0.0;
         this.withdrawalLimit = 0.0;
         this.transactionLimit = 0;
         this.maintenanceFee = 0.0;
+        this.transactionCount = 0;
     }
     public CurrentAccount(double overdraftLimit,double withdrawalLimit,int transactionLimit,double maintenanceFee,String accNumber, double balance, String ifscCode, String branchName, Customer accountHolder){
         super(accNumber,balance,ifscCode,branchName,accountHolder);
@@ -20,6 +22,7 @@ public class CurrentAccount extends Account{
         this.withdrawalLimit = withdrawalLimit;
         this.transactionLimit = transactionLimit;
         this.maintenanceFee = maintenanceFee;
+        this.transactionCount = 0;
     }
     public double getOverdraftLimit(){
         return this.overdraftLimit;
@@ -45,6 +48,9 @@ public class CurrentAccount extends Account{
     public void setMaintenanceFee(double newMaintenanceFee){
         this.maintenanceFee = newMaintenanceFee;
     }
+    public int getTransactionCount(){
+        return transactionCount;
+    }
     @Override
     public void displayDetails(){
         super.displayDetails();
@@ -52,6 +58,14 @@ public class CurrentAccount extends Account{
         System.out.println("Withdrawal Limit: " + getWithdrawalLimit());
         System.out.println("Transaction Limit: " + getTransactionLimit());
         System.out.println("Maintenance Fee: " + getMaintenanceFee());
+        System.out.println("Transaction Count: " + getTransactionCount());
+    }
+    @Override
+    public boolean withdraw(double amount){
+        if(amount <= 0 || amount>getWithdrawalLimit() || getTransactionCount() >= getTransactionLimit() || getBalance() - amount < -(getOverdraftLimit())) return false;
+        setBalance(getBalance()-amount);
+        this.transactionCount++;
+        return true;
     }
 }
 
