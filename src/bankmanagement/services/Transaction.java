723201c009a440c3f9fc516d3af1a332833baf69
Ankita <ankita180206@gmail.com>
2026-07-27 20:20:55 +0530

    Update Transaction.java

--- src/bankmanagement/services/Transaction.java
+++ src/bankmanagement/services/Transaction.java
@@ -1,4 +1,65 @@
 package bankmanagement.services;
+import bankmanagement.enums.*;
+import bankmanagement.accounts.*;
 //Defines the operations that an account should support.
-public interface Transaction {
+public class Transaction {
+    private String transactionId;
+    private double amount;
+    private TransactionType type;
+    private TransactionStatus status;
+    private Account account;
+    public Transaction(){
+        this.transactionId = "No ID";
+        this.amount = 0.0;
+        this.type = TransactionType.NOTRANSACTION;
+        this.status = TransactionStatus.PENDING;
+        this.account = null;
+    }
+    public Transaction(String transactionId, double amount, TransactionType type, TransactionStatus status, Account account){
+        this.transactionId = transactionId;
+        this.amount = amount;
+        this.type = type;
+        this.status = status;
+        this.account = account;
+    }
+    public String getTransactionId(){
+        return transactionId;
+    }
+    public void setTransactionId(String newTransactionId){
+        this.transactionId = newTransactionId;
+    }
+    public double getAmount(){
+        return amount;
+    }
+    public void setAmount(double newAmount){
+        this.amount = newAmount;
+    }
+    public TransactionType getType(){
+        return type;
+    }
+    public void setType(TransactionType newType){
+        this.type = newType;
+    }
+    public TransactionStatus getStatus(){
+        return status;
+    }
+    public void setStatus(TransactionStatus newStatus){
+        this.status = newStatus;
+    }
+    public Account getAccount(){
+        return account;
+    }
+    public void setAccount(Account newAccount){
+        this.account = newAccount;
+    }
+    public void displayDetails(){
+        if(account != null){
+            System.out.println("Account number: " + account.getAccNumber());
+        }else{
+            System.out.println("Account number: Not Assigned");
+        }
+        System.out.println("Transaction ID: "+getTransactionId());
+        System.out.println("Transaction type: "+getType());
+        System.out.println("Transaction Status: "+ getStatus());
+    }
 }
